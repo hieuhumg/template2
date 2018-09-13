3b4b1389321e6e1fb99b562755ea8fd9eb0da4bd
= <=>
2018-09-13 22:48:03 +0700

    firs commit

---
+++ js/owlcarousel/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:15,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        1000:{
+	            items:3
+	        },
+	        1500:{
+	            items:5
+	        }
+	    }
+	})
+});
+
